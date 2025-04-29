ec71a72086dcf6d68e9045da875ff90ffcc41a59
Bibek Tamrakar <bibek.tamrakar@msn.com>
2025-04-29 11:19:58 +0545

    Updated files to display properly

--- src/main/java/com/example/datetimeapp/DateTimeController.java
+++ src/main/java/com/example/datetimeapp/DateTimeController.java
@@ -1,9 +1,11 @@
 package com.example.datetimeapp;
 
+import java.time.LocalDateTime;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import java.time.LocalDateTime;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class DateTimeController {
@@ -11,6 +13,11 @@ public class DateTimeController {
     public String showDateTime(Model model) {
         model.addAttribute("currentDateTime", LocalDateTime.now());
         return "datetime";
+    }
+    @GetMapping("/current-time")
+    @ResponseBody
+    public String getCurrentTime() {
+        return LocalDateTime.now().toString();
     }
 }
 
