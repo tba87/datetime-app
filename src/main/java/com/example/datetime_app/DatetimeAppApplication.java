e6fe2cb76fd3a6395182f87383d3fbbc0b170c12
Bibek Tamrakar <bibek.tamrakar@msn.com>
2025-04-29 10:13:50 +0545

    first commit

---
+++ src/main/java/com/example/datetime_app/DatetimeAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.datetime_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DatetimeAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DatetimeAppApplication.class, args);
+	}
+
+}
